12a5540d6773cfc00a75656f32ec0a5fcb3fb43f
unknown <zhoumilau@gmail.com>
2024-06-15 20:11:57 +0800

    add feature login

---
+++ src/components/layouts/Navbar/index.tsx
@@ -0,0 +1,17 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import styles from "./Navbar.module.scss";
+function Navbar() {
+  const { data } = useSession();
+  return (
+    <div className={styles.navbar}>
+      <button
+        className={styles.navbar__button}
+        onClick={() => (data ? signOut() : signIn())}
+      >
+        {data ? "Logout" : "Login"}
+      </button>
+    </div>
+  );
+}
+
+export default Navbar;
